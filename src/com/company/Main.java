8d5334db561ddd7d2198bd7d898ca8d219d0888f
danisz118 <daniz118@gmail.com>
2020-03-15 12:22:54 +0200

    add check method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,36 @@
 package com.company;
+/*
+
+ */
 
 import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Insert method: plus , minus , multiply, division  ");
         String methodEnter = scanner.nextLine();
-        Math math = new Math();
+        if (methodEnter.equals("plus") || methodEnter.equals("minus")
+                || methodEnter.equals("multiply")
+                || methodEnter.equals("division")) {
+            Math math = new Math();
 
-        try {
-            reflection(math,methodEnter);
-        } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                reflection(math, methodEnter);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            System.out.println("no such method");
         }
+
     }
-    public static void reflection(Math math, String methodEnter) throws Exception{
-        Method method= math.getClass().getMethod(methodEnter);
+
+    public static void reflection(Math math, String methodEnter) throws Exception {
+        Method method = math.getClass().getMethod(methodEnter);
         method.invoke(math);
 
     }
